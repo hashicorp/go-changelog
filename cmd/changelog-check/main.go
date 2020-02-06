f1977950970e81ce3cb44f5c1278c4afa2bffc25
Paddy Carver <paddy@hashicorp.com>
2020-02-06 02:59:06 -0800

    Revert changelog-check to using PR body.
    I forgot that we're going to be running changelog-check against
    magic-modules, which won't have the changelog file. Instead, the
    changelog entries will be in the body, and the magician will generate
    the changelog files on the downstream before it gets merged. So rather
    than having changelog-check look for the files, let's have it look in
    the PR body, which is also much simpler and less code, which is nice.

--- cmd/changelog-check/main.go
+++ cmd/changelog-check/main.go
@@ -2,12 +2,9 @@ package main
 
 import (
 	"context"
-	"io/ioutil"
 	"log"
-	"net/http"
 	"os"
 	"strconv"
-	"strings"
 
 	"github.com/google/go-github/github"
 	"github.com/hashicorp/go-changelog"
@@ -16,7 +13,7 @@ import (
 func main() {
 	ctx := context.Background()
 	pr := os.Args[1]
-	prNo, err := strconv.ParseInt(pr, 10, 64)
+	prNo, err := strconv.Atoi(pr)
 	if err != nil {
 		log.Fatalf("Error parsing PR %q as a number: %w", pr, err)
 	}
@@ -33,57 +30,14 @@ func main() {
 
 	client := github.NewClient(nil)
 
-	var file *github.CommitFile
-	opt := &github.ListOptions{
-		PerPage: 100,
-	}
-	for {
-		commitFiles, resp, err := client.PullRequests.ListFiles(ctx,
-			owner, repo, int(prNo), opt)
-		if err != nil {
-			log.Fatalf("Error retrieving pull request github.com/"+
-				"%s/%s/%d: %w", owner, repo, prNo, err)
-		}
-		for _, f := range commitFiles {
-			if strings.HasPrefix(f.GetFilename(), ".changelog/") {
-				if file != nil {
-					log.Fatalf("Two changelog files found for PR %s: %w and %w",
-						f.GetFilename(), file.GetFilename())
-				}
-				file = f
-				break
-			}
-		}
-		if file != nil {
-			break
-		}
-		if resp.NextPage == 0 {
-			break
-		}
-		opt.Page = resp.NextPage
-	}
-	if file == nil {
-		log.Fatalf("No changelog file found for PR %s", pr)
-	}
-	resp, err := http.Get(file.GetRawURL())
+	pullRequest, _, err := client.PullRequests.Get(ctx, owner, repo, prNo)
 	if err != nil {
-		log.Fatalf("Error retrieving changelog file contents from %q: %w",
-			file.GetRawURL(), err)
+		log.Fatalf("Error retrieving pull request github.com/"+
+			"%s/%s/%d: %w", owner, repo, prNo, err)
 	}
-	body, err := readResponse(resp)
-	if err != nil {
-		log.Fatalf("Error reading changelog file contents from %q: %w",
-			file.GetRawURL(), err)
-	}
-	if resp.StatusCode != http.StatusOK {
-		log.Fatalf("HTTP error reading changelog file contents from %q: %d %s",
-			file.GetRawURL(), resp.StatusCode, body)
-	}
-	issue := strings.TrimPrefix(file.GetFilename(), ".changelog/")
-	issue = strings.TrimSuffix(issue, ".txt")
 	entry := changelog.Entry{
-		Issue: issue,
-		Body:  string(body),
+		Issue: pr,
+		Body:  pullRequest.GetBody(),
 	}
 	notes := changelog.NotesFromEntry(entry)
 	if len(notes) < 1 {
@@ -105,9 +59,4 @@ func main() {
 			log.Fatalf("unknown changelog type %q", note.Type)
 		}
 	}
-}
-
-func readResponse(resp *http.Response) ([]byte, error) {
-	defer resp.Body.Close()
-	return ioutil.ReadAll(resp.Body)
 }
