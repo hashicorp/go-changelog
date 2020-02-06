bc4527797f393ba2a3ac2dfa18dd8040f8ddd698
Paddy Carver <paddy@hashicorp.com>
2020-02-06 02:32:51 -0800

    Flesh out changelog-check.
    Make changelog-check download the PR from GitHub, have it find the
    changelog file (if there is one!) and check it against our allowed types
    of changelog files.
    
    This still needs to:
    
    * Offer better configuration options--an HCL config file and parser
      seems appropriate for configuring rules
    * Post comments/reviews on things that don't meet the guidelines.

--- cmd/changelog-check/main.go
+++ cmd/changelog-check/main.go
@@ -1,41 +1,113 @@
 package main
 
 import (
-	"fmt"
+	"context"
 	"io/ioutil"
+	"log"
+	"net/http"
 	"os"
+	"strconv"
+	"strings"
 
+	"github.com/google/go-github/github"
 	"github.com/hashicorp/go-changelog"
 )
 
 func main() {
-	in, err := ioutil.ReadAll(os.Stdin)
+	ctx := context.Background()
+	pr := os.Args[1]
+	prNo, err := strconv.ParseInt(pr, 10, 64)
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		log.Fatalf("Error parsing PR %q as a number: %w", pr, err)
 	}
+
+	owner := os.Getenv("GITHUB_OWNER")
+	repo := os.Getenv("GITHUB_REPO")
+
+	if owner == "" {
+		log.Fatalf("GITHUB_OWNER not set")
+	}
+	if repo == "" {
+		log.Fatalf("GITHUB_REPO not set")
+	}
+
+	client := github.NewClient(nil)
+
+	var file *github.CommitFile
+	opt := &github.ListOptions{
+		PerPage: 100,
+	}
+	for {
+		commitFiles, resp, err := client.PullRequests.ListFiles(ctx,
+			owner, repo, int(prNo), opt)
+		if err != nil {
+			log.Fatalf("Error retrieving pull request github.com/"+
+				"%s/%s/%d: %w", owner, repo, prNo, err)
+		}
+		for _, f := range commitFiles {
+			if strings.HasPrefix(f.GetFilename(), ".changelog/") {
+				if file != nil {
+					log.Fatalf("Two changelog files found for PR %s: %w and %w",
+						f.GetFilename(), file.GetFilename())
+				}
+				file = f
+				break
+			}
+		}
+		if file != nil {
+			break
+		}
+		if resp.NextPage == 0 {
+			break
+		}
+		opt.Page = resp.NextPage
+	}
+	if file == nil {
+		log.Fatalf("No changelog file found for PR %s", pr)
+	}
+	resp, err := http.Get(file.GetRawURL())
+	if err != nil {
+		log.Fatalf("Error retrieving changelog file contents from %q: %w",
+			file.GetRawURL(), err)
+	}
+	body, err := readResponse(resp)
+	if err != nil {
+		log.Fatalf("Error reading changelog file contents from %q: %w",
+			file.GetRawURL(), err)
+	}
+	if resp.StatusCode != http.StatusOK {
+		log.Fatalf("HTTP error reading changelog file contents from %q: %d %s",
+			file.GetRawURL(), resp.StatusCode, body)
+	}
+	issue := strings.TrimPrefix(file.GetFilename(), ".changelog/")
+	issue = strings.TrimSuffix(issue, ".txt")
 	entry := changelog.Entry{
-		Issue: os.Args[1],
-		Body:  string(in),
+		Issue: issue,
+		Body:  string(body),
 	}
 	notes := changelog.NotesFromEntry(entry)
 	if len(notes) < 1 {
-		fmt.Println("no changelog entry")
-		os.Exit(1)
+		log.Fatalf("no changelog entry found in %s: %s", entry.Issue,
+			string(entry.Body))
 	}
 	for _, note := range notes {
 		switch note.Type {
-		case "bug",
-			"improvement",
-			"feature",
-			"breaking-change",
+		case "none",
+			"bug",
+			"note",
+			"enhancement",
 			"new-resource",
 			"new-datasource",
-			"new-data-source",
-			"deprecation":
+			"deprecation",
+			"breaking-change",
+			"feature":
 		default:
-			fmt.Fprintf(os.Stderr, "unknown changelog type %q\n", note.Type)
-			os.Exit(1)
+			log.Fatalf("unknown changelog type %q", note.Type)
 		}
 	}
+}
+
+func readResponse(resp *http.Response) ([]byte, error) {
+	defer resp.Body.Close()
+	return ioutil.ReadAll(resp.Body)
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/hashicorp/go-changelog
 go 1.13
 
 require (
+	github.com/google/go-github v17.0.0+incompatible
+	github.com/google/go-querystring v1.0.0 // indirect
 	golang.org/x/sys v0.0.0-20191008105621-543471e840be // indirect
 	gopkg.in/src-d/go-billy.v4 v4.3.2
 	gopkg.in/src-d/go-git.v4 v4.13.1
