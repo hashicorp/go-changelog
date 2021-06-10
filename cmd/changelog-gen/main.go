a7431a52f6275b7109f434e49b5ca1b2c707e79a
Dhia Ayachi <dhia@hashicorp.com>
2021-06-10 16:07:40 -0400

    add interactive mode and fix issue with git when fetching all remotes

--- cmd/changelog-gen/main.go
+++ cmd/changelog-gen/main.go
@@ -9,6 +9,7 @@ import (
 	"fmt"
 	"github.com/go-git/go-git/v5"
 	"github.com/google/go-github/github"
+	"github.com/manifoldco/promptui"
 	"os"
 	"regexp"
 	"strings"
@@ -56,7 +57,7 @@ func main() {
 	if pr == -1 {
 		pr, url, err = getPrNumberFromGithub(pwd)
 		if err != nil {
-			fmt.Fprintln(os.Stderr, "Must specify pr number of run in a valid github repo")
+			fmt.Fprintln(os.Stderr, "Must specify pr number or run in a valid github repo")
 			fmt.Fprintln(os.Stderr, "")
 			flag.Usage()
 			os.Exit(1)
@@ -64,17 +65,37 @@ func main() {
 	}
 
 	if Type == "" {
-		fmt.Fprintln(os.Stderr, "Must specify the change type")
-		fmt.Fprintln(os.Stderr, "")
-		flag.Usage()
-		os.Exit(1)
+		prompt := promptui.Select{
+			Label: "Select a change type",
+			Items: []string{"enhancement",
+				"bug",
+				"note",
+				"new-resource",
+				"new-datasource",
+				"deprecation",
+				"breaking-change",
+				"none"},
+		}
+
+		_, Type, err = prompt.Run()
+
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Must specify the change type")
+			fmt.Fprintln(os.Stderr, "")
+			flag.Usage()
+			os.Exit(1)
+		}
 	}
 
 	if Description == "" {
-		fmt.Fprintln(os.Stderr, "Must specify the change description")
-		fmt.Fprintln(os.Stderr, "")
-		flag.Usage()
-		os.Exit(1)
+		prompt := promptui.Prompt{Label: "Description"}
+		Description, err = prompt.Run()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Must specify the change description")
+			fmt.Fprintln(os.Stderr, "")
+			flag.Usage()
+			os.Exit(1)
+		}
 	}
 	var tmpl *template.Template
 	if changelogTmpl != "" {
@@ -125,20 +146,19 @@ func OpenGit(path string) (*git.Repository, error) {
 func getPrNumberFromGithub(path string) (int, string, error) {
 	r, err := OpenGit(path)
 	if err != nil {
-		return 0, "", err
+		return -1, "", err
 	}
 
 	ref, err := r.Head()
 	if err != nil {
-		return 0, "", err
+		return -1, "", err
 	}
 
-	rem, err := r.Remotes()
+	rem, err := r.Remote("origin")
 	if err != nil {
-		return 0, "", err
+		return -1, "", err
 	}
 
-	ref.Target()
 	cli := github.NewClient(nil)
 
 	opt := &github.PullRequestListOptions{
@@ -148,16 +168,27 @@ func getPrNumberFromGithub(path string) (int, string, error) {
 	}
 
 	ctx := context.Background()
-	repoUrl := rem[0].Config().URLs[0]
+	if len(rem.Config().URLs) <= 0 {
+		return -1, "", errors.New("not able to parse repo and org")
+	}
+	repoUrl := rem.Config().URLs[0]
 
 	reg := regexp.MustCompile(".*github\\.com:(.*)/(.*)\\.git")
 	m := reg.FindAllStringSubmatch(repoUrl, -1)
+	if len(m) > 1 {
+		if len(m[0]) < 2 {
+			return -1, "", errors.New("not able to parse repo and org")
+		}
+	}
 
 	list, _, err := cli.PullRequests.List(ctx, m[0][1], m[0][2], opt)
 	if err != nil {
-		return 0, "", err
+		return -1, "", err
 	}
-
+	branchName := ref.Name().Short()
+	if branchName == "master" || branchName == "main" {
+		return -1, "", errors.New("cannot find a pr # for the main branch")
+	}
 	for _, pr := range list {
 		if pr.Head == nil {
 			continue
@@ -165,7 +196,7 @@ func getPrNumberFromGithub(path string) (int, string, error) {
 		if pr.Head.Ref == nil {
 			continue
 		}
-		branchName := ref.Name().Short()
+
 		if *pr.Head.Ref == branchName {
 			n := pr.GetNumber()
 			if n != 0 {
@@ -173,5 +204,5 @@ func getPrNumberFromGithub(path string) (int, string, error) {
 			}
 		}
 	}
-	return 0, "", errors.New("not found")
+	return -1, "", errors.New("not found")
 }
