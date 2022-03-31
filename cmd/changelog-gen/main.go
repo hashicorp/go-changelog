db75d7b12413a94b48733ab4af80cdcd36f6d46f
Mike Morris <mikemorris@users.noreply.github.com>
2022-03-31 16:16:18 -0400

    changelog-gen: minor cleanup of getPrNumberFomrGitHub, changelog entry template and help text, add subcategory prompt

--- cmd/changelog-gen/main.go
+++ cmd/changelog-gen/main.go
@@ -17,18 +17,19 @@ import (
 	"text/template"
 )
 
-//go:embed changelog.tmpl
-var changelogTplDefault string
+//go:embed changelog-entry.tmpl
+var changelogTmplDefault string
 
 type Note struct {
-	// service touched by pr
-	Service string
-	//release note type (Bug...)
+	// service or area of codebase the pull request changes
+	Subcategory string
+	// release note type (Bug...)
 	Type string
 	// release note text
 	Description string
-	//pr number
-	Pr  int
+	// pull request number
+	PR int
+	// URL of the pull request
 	URL string
 }
 
@@ -38,34 +39,35 @@ func main() {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
-	var service, Type, Description, changelogTmpl, url string
+	var subcategory, changeType, description, changelogTmpl, url string
 	var pr int
 	var Url bool
-	flag.BoolVar(&Url, "add-url", false, "do not add github issue url")
-	flag.IntVar(&pr, "pr", -1, "pr number")
-	flag.StringVar(&service, "service", "", "the service the pr change (not mandatory)")
-	flag.StringVar(&Type, "type", "", "The pr type")
-	flag.StringVar(&Description, "description", "", "the changelog-gen description entry")
-	flag.StringVar(&changelogTmpl, "changelog-template", "", "the path of the file holding the template to use for the entire changelog-gen")
+	flag.BoolVar(&Url, "add-url", false, "add GitHub issue URL (omitted by default due to formatting in changelog-build)")
+	flag.IntVar(&pr, "pr", -1, "pull request number")
+	flag.StringVar(&subcategory, "subcategory", "", "the service or area of the codebase the pull request changes (optional)")
+	flag.StringVar(&changeType, "type", "", "the type of change")
+	flag.StringVar(&description, "description", "", "the changelog entry content")
+	flag.StringVar(&changelogTmpl, "changelog-template", "", "the path of the file holding the template to use for the changelog entries")
 	flag.Parse()
 
 	if pr == -1 {
 		pr, url, err = getPrNumberFromGithub(pwd)
 		if err != nil {
-			fmt.Fprintln(os.Stderr, "Must specify pr number or run in a valid github repo")
+			fmt.Fprintln(os.Stderr, "Must specify pull request number or run in a git repo with a GitHub remote origin:", err)
 			fmt.Fprintln(os.Stderr, "")
 			flag.Usage()
 			os.Exit(1)
 		}
 	}
+	fmt.Fprintln(os.Stderr, "Found matching pull request:", url)
 
-	if Type == "" {
+	if changeType == "" {
 		prompt := promptui.Select{
 			Label: "Select a change type",
 			Items: changelog.TypeValues,
 		}
 
-		_, Type, err = prompt.Run()
+		_, changeType, err = prompt.Run()
 
 		if err != nil {
 			fmt.Fprintln(os.Stderr, "Must specify the change type")
@@ -74,7 +76,7 @@ func main() {
 			os.Exit(1)
 		}
 	} else {
-		if !changelog.TypeValid(Type) {
+		if !changelog.TypeValid(changeType) {
 			fmt.Fprintln(os.Stderr, "Must specify a valid type")
 			fmt.Fprintln(os.Stderr, "")
 			flag.Usage()
@@ -82,9 +84,14 @@ func main() {
 		}
 	}
 
-	if Description == "" {
+	if subcategory == "" {
+		prompt := promptui.Prompt{Label: "Subcategory (optional)"}
+		subcategory, err = prompt.Run()
+	}
+
+	if description == "" {
 		prompt := promptui.Prompt{Label: "Description"}
-		Description, err = prompt.Run()
+		description, err = prompt.Run()
 		if err != nil {
 			fmt.Fprintln(os.Stderr, "Must specify the change description")
 			fmt.Fprintln(os.Stderr, "")
@@ -92,6 +99,7 @@ func main() {
 			os.Exit(1)
 		}
 	}
+
 	var tmpl *template.Template
 	if changelogTmpl != "" {
 		file, err := os.ReadFile(changelogTmpl)
@@ -103,7 +111,7 @@ func main() {
 			os.Exit(1)
 		}
 	} else {
-		tmpl, err = template.New("").Parse(changelogTplDefault)
+		tmpl, err = template.New("").Parse(changelogTmplDefault)
 		if err != nil {
 			os.Exit(1)
 		}
@@ -112,7 +120,7 @@ func main() {
 	if !Url {
 		url = ""
 	}
-	n := Note{Type: Type, Description: Description, Service: service, Pr: pr, URL: url}
+	n := Note{Type: changeType, Description: description, Subcategory: subcategory, PR: pr, URL: url}
 
 	var buf bytes.Buffer
 	err = tmpl.Execute(&buf, n)
@@ -120,10 +128,12 @@ func main() {
 	if err != nil {
 		os.Exit(1)
 	}
-	err = os.WriteFile(fmt.Sprintf("%s/%d.txt", pwd, pr), buf.Bytes(), 0644)
+	filepath := fmt.Sprintf("%s/%d.txt", pwd, pr)
+	err = os.WriteFile(filepath, buf.Bytes(), 0644)
 	if err != nil {
 		os.Exit(1)
 	}
+	fmt.Fprintln(os.Stderr, "Created changelog entry at", filepath)
 }
 
 func OpenGit(path string) (*git.Repository, error) {
@@ -149,12 +159,33 @@ func getPrNumberFromGithub(path string) (int, string, error) {
 		return -1, "", err
 	}
 
-	rem, err := r.Remote("origin")
+	localBranch, err := r.Branch(ref.Name().Short())
 	if err != nil {
 		return -1, "", err
 	}
 
+	remote, err := r.Remote("origin")
+	if err != nil {
+		return -1, "", err
+	}
+
+	if len(remote.Config().URLs) <= 0 {
+		return -1, "", errors.New("not able to parse repo and org")
+	}
+	remoteUrl := remote.Config().URLs[0]
+
+	re := regexp.MustCompile(`.*github\.com:(.*)/(.*)\.git`)
+	m := re.FindStringSubmatch(remoteUrl)
+	if len(m) < 3 {
+		return -1, "", errors.New("not able to parse repo and org")
+	}
+
 	cli := github.NewClient(nil)
+
+	ctx := context.Background()
+
+	githubOrg := m[1]
+	githubRepo := m[2]
 
 	opt := &github.PullRequestListOptions{
 		ListOptions: github.ListOptions{PerPage: 200},
@@ -162,42 +193,46 @@ func getPrNumberFromGithub(path string) (int, string, error) {
 		Direction:   "desc",
 	}
 
-	ctx := context.Background()
-	if len(rem.Config().URLs) <= 0 {
-		return -1, "", errors.New("not able to parse repo and org")
-	}
-	repoUrl := rem.Config().URLs[0]
-
-	reg := regexp.MustCompile(`.*github\\.com:(.*)/(.*)\\.git`)
-	m := reg.FindAllStringSubmatch(repoUrl, -1)
-	if len(m) > 1 {
-		if len(m[0]) < 2 {
-			return -1, "", errors.New("not able to parse repo and org")
-		}
-	}
-
-	list, _, err := cli.PullRequests.List(ctx, m[0][1], m[0][2], opt)
+	list, _, err := cli.PullRequests.List(ctx, githubOrg, githubRepo, opt)
 	if err != nil {
 		return -1, "", err
 	}
-	branchName := ref.Name().Short()
-	if branchName == "master" || branchName == "main" {
-		return -1, "", errors.New("cannot find a pr # for the main branch")
-	}
+
 	for _, pr := range list {
-		if pr.Head == nil {
-			continue
-		}
-		if pr.Head.Ref == nil {
+		head := pr.GetHead()
+		if head == nil {
 			continue
 		}
 
-		if *pr.Head.Ref == branchName {
+		branch := head.GetRef()
+		if branch == "" {
+			continue
+		}
+
+		repo := head.GetRepo()
+		if repo == nil {
+			continue
+		}
+
+		// Allow finding PRs from forks - localBranch.Remote will return the
+		// remote name for branches of origin, but the remote URL for forks
+		var gitRemote string
+		remote, err := r.Remote(localBranch.Remote)
+		if err != nil {
+			gitRemote = localBranch.Remote
+		} else {
+			gitRemote = remote.Config().URLs[0]
+		}
+
+		if (gitRemote == *repo.SSHURL || gitRemote == *repo.CloneURL) &&
+			localBranch.Name == branch {
 			n := pr.GetNumber()
+
 			if n != 0 {
 				return n, pr.GetHTMLURL(), nil
 			}
 		}
 	}
-	return -1, "", errors.New("not found")
+
+	return -1, "", errors.New("no match found")
 }
