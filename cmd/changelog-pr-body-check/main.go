95d32b928292ad9dfbe9d3f10ce964c24e17557b
Mike Morris <mikemorris@users.noreply.github.com>
2022-04-07 10:36:44 -0400

    changelog-pr-body-check: refactor to use Entry.Validate

--- cmd/changelog-pr-body-check/main.go
+++ cmd/changelog-pr-body-check/main.go
@@ -48,60 +48,57 @@ func main() {
 		log.Fatalf("Error retrieving pull request github.com/"+
 			"%s/%s/%d: %s", owner, repo, prNo, err)
 	}
+
 	entry := changelog.Entry{
 		Issue: pr,
 		Body:  pullRequest.GetBody(),
 	}
-	notes := changelog.NotesFromEntry(entry)
-	if len(notes) < 1 {
-		log.Printf("no changelog entry found in %s: %s", entry.Issue,
-			string(entry.Body))
-		body := "Oops! It looks like no changelog entry is attached to" +
-			" this PR. Please include a release note block" +
-			" in the PR body, as described in https://github.com/GoogleCloudPlatform/magic-modules/blob/master/.ci/RELEASE_NOTES_GUIDE.md:" +
-			"\n\n~~~\n```release-note:TYPE\nRelease note" +
-			"\n```\n~~~"
-		_, _, err = client.Issues.CreateComment(ctx, owner, repo,
-			prNo, &github.IssueComment{
-				Body: &body,
-			})
-		if err != nil {
-			log.Fatalf("Error creating pull request comment on"+
-				" github.com/%s/%s/%d: %s", owner, repo, prNo,
-				err)
+
+	if err := entry.Validate(); err != nil {
+		log.Printf("error parsing changelog entry in %s: %s", entry.Issue, err)
+		switch err.Code {
+		case changelog.EntryErrorNotFound:
+			body := "Oops! It looks like no changelog entry is attached to" +
+				" this PR. Please include a release note block" +
+				" in the PR body, as described in https://github.com/GoogleCloudPlatform/magic-modules/blob/master/.ci/RELEASE_NOTES_GUIDE.md:" +
+				"\n\n~~~\n```release-note:TYPE\nRelease note" +
+				"\n```\n~~~"
+			_, _, err := client.Issues.CreateComment(ctx, owner, repo,
+				prNo, &github.IssueComment{
+					Body: &body,
+				})
+			if err != nil {
+				log.Fatalf("Error creating pull request comment on"+
+					" github.com/%s/%s/%d: %s", owner, repo, prNo,
+					err)
+			}
+			os.Exit(1)
+		case changelog.EntryErrorUnknownTypes:
+			unknownTypes := err.Details["unknownTypes"].([]string)
+
+			body := "Oops! It looks like you're using"
+			if len(unknownTypes) == 1 {
+				body += " an"
+			}
+			body += " unknown release-note type"
+			if len(unknownTypes) > 1 {
+				body += "s"
+			}
+			body += " in your changelog entries:"
+			for _, t := range unknownTypes {
+				body += "\n* " + t
+			}
+			body += "\n\nPlease only use the types listed in https://github.com/GoogleCloudPlatform/magic-modules/blob/master/.ci/RELEASE_NOTES_GUIDE.md."
+			_, _, err := client.Issues.CreateComment(ctx, owner, repo,
+				prNo, &github.IssueComment{
+					Body: &body,
+				})
+			if err != nil {
+				log.Fatalf("Error creating pull request comment on"+
+					" github.com/%s/%s/%d: %s", owner, repo, prNo,
+					err)
+			}
+			os.Exit(1)
 		}
-		os.Exit(1)
-	}
-	var unknownTypes []string
-	for _, note := range notes {
-		if !changelog.TypeValid(note.Type) {
-			unknownTypes = append(unknownTypes, note.Type)
-		}
-	}
-	if len(unknownTypes) > 0 {
-		log.Printf("unknown changelog types %v", unknownTypes)
-		body := "Oops! It looks like you're using"
-		if len(unknownTypes) == 1 {
-			body += " an"
-		}
-		body += " unknown release-note type"
-		if len(unknownTypes) > 1 {
-			body += "s"
-		}
-		body += " in your changelog entries:"
-		for _, t := range unknownTypes {
-			body += "\n* " + t
-		}
-		body += "\n\nPlease only use the types listed in https://github.com/GoogleCloudPlatform/magic-modules/blob/master/.ci/RELEASE_NOTES_GUIDE.md."
-		_, _, err = client.Issues.CreateComment(ctx, owner, repo,
-			prNo, &github.IssueComment{
-				Body: &body,
-			})
-		if err != nil {
-			log.Fatalf("Error creating pull request comment on"+
-				" github.com/%s/%s/%d: %s", owner, repo, prNo,
-				err)
-		}
-		os.Exit(1)
 	}
 }
